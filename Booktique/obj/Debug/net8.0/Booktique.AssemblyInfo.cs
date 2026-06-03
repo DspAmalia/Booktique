631827f2f276f0846e129b9477f24e0f695bc7e5
DspAmalia <dspamalia@gmail.com>
2026-06-03 19:55:43 +0300

    dashboard

--- Booktique/obj/Debug/net8.0/Booktique.AssemblyInfo.cs
+++ Booktique/obj/Debug/net8.0/Booktique.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Booktique")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1d6b9b19090aeb40798eed955b7faa3d838b08a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4556031a0eb482b3daa06cb03d649ac57073ac6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Booktique")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Booktique")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
